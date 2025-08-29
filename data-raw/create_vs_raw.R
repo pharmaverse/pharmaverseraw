2bafbdc41f7042ce2edc4e56104116da95d02e89
Rammprasad Ganapathy <ganapathy.rammprasad@gene.com>
2025-08-29 20:00:51 +0000

    Vital signs update

--- data-raw/create_vs_raw.R
+++ data-raw/create_vs_raw.R
@@ -5,22 +5,16 @@
 sdtm_vs <- pharmaversesdtm::vs
 
 vs_raw1 <- sdtm_vs |>
-  dplyr::filter(VSTESTCD %in% c("HEIGHT", "WEIGHT", "TEMP")) |>
+  dplyr::arrange(USUBJID,VSDTC, VSTPTNUM) |>
+  dplyr::select(STUDYID, USUBJID, VISIT, VSDTC, VSTPT, VSSTAT, VSLOC, VSPOS,
+                VSTESTCD, VSORRES) |>
   tidyr::pivot_wider(
-    id_cols = c(STUDYID, USUBJID, VISIT, VSDTC),
+    id_cols = c(STUDYID, USUBJID, VISIT, VSDTC, VSTPT, VSSTAT, VSLOC, VSPOS),
     names_from = VSTESTCD,
     values_from = VSORRES
   )
 
-vs_raw2 <- sdtm_vs |>
-  dplyr::filter(!VSTESTCD %in% c("HEIGHT", "WEIGHT", "TEMP")) |>
-  tidyr::pivot_wider(
-    id_cols = c(STUDYID, USUBJID, VSPOS, VSLOC, VISIT, VSDTC, VSTPT, VSTPTREF),
-    names_from = VSTESTCD,
-    values_from = VSORRES
-  )
-
-vs_raw <- dplyr::left_join(vs_raw2, vs_raw1) |>
+vs_raw <- vs_raw1a |>
   dplyr::mutate(
     STUDY = "CDISCPILOT01",
     PATNUM = substr(USUBJID, 4, 11),
@@ -28,16 +22,24 @@ vs_raw <- dplyr::left_join(vs_raw2, vs_raw1) |>
     INSTANCE = gsub("Ecg", "ECG", INSTANCE),
     FORM = "VS",
     FORML = "Vital Signs",
-    TMPTC = tools::toTitleCase(tolower(VSTPT)),
     VTLD = format(as.Date(VSDTC, format = "%Y-%m-%d"), "%d-%b-%Y"),
-    SUBPOS = tolower(VSPOS),
+    IT.HEIGHT_VSORRES = HEIGHT,
+    IT.WEIGHT = WEIGHT,
+    IT.TEMP = TEMP,
+    IT.TEMP_LOC = dplyr::case_when(is.na(TEMP) ~ NA_character_,
+      !is.na(TEMP)  ~ VSLOC,
+      TRUE ~ NA_character_),
+    TMPTC = tools::toTitleCase(tolower(VSTPT)),
     SYS_BP = SYSBP,
     DIA_BP = DIABP,
-    LOC = VSLOC
+    SUBPOS = dplyr::case_when(is.na(TEMP) ~ VSPOS,
+                       !is.na(TEMP)  ~ NA_character_,
+                       TRUE ~ NA_character_)
   ) |>
   dplyr::select(
-    STUDY, PATNUM, INSTANCE, FORM, FORML, TMPTC, VTLD,
-    SUBPOS, LOC, SYS_BP, DIA_BP, HEIGHT, WEIGHT, TEMP, PULSE
+    STUDY, PATNUM, INSTANCE, FORM, FORML, VTLD, IT.HEIGHT_VSORRES,
+    IT.WEIGHT, IT.TEMP, IT.TEMP_LOC, TMPTC, SYS_BP, DIA_BP,
+    PULSE, SUBPOS
   )
 
 for (col in names(vs_raw)) {
